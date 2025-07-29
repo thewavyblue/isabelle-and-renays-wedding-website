31438418e7564a27db1ca6bd27ad7da35f1b9fb8
Ben <thewavyblue@gmail.com>
2025-07-29 19:37:32 +0100

    v0.3

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         Welcome to the wed-site of
       </p>
       <Divider />
-      <h1>
+      <h1 style={{marginLeft: 9}}>
         Isabelle<br />
         &<br />
         Renay
@@ -22,6 +22,116 @@ function App() {
       <Divider />
       <Countdown />
       <Button link={"#intro"} buttonLabel={"Jump to info"}/>
+      <div className="wrapper__content">
+        <p className="p-lrg">
+          We joyfully request the pleasure of your company as we speak our vows and join in marriage in the presence of family and friends.
+        </p>
+        <p>
+          The Oak Barn in Cranbrook will be our venue hosting the ceremony, breakfast, and evening celebrations. One destination that does it all.
+        </p>
+      </div>
+      <Divider />
+      <div className="wrapper__content">
+        <h2>
+          When
+        </h2>
+        <p className="p-lrg">
+          Saturday 19th September 2026
+        </p>
+        <Button link={"#intro"} buttonLabel={"Save the date (iOS)"}/>
+        <Button link={"#intro"} buttonLabel={"Save the date (Android)"}/>
+      </div>
+      <Divider />
+      <div className="wrapper__content">
+        <h2>
+          Where
+        </h2>
+        <p className="p-lrg">
+          The Oak Barn, Frame Farm
+        </p>
+        <p>
+          Iden Green Road,<br />
+          Benenden, Iden Green,<br />
+          Cranbrook<br />
+          TN17 4EZ
+        </p>
+        <Button link={"#intro"} buttonLabel={"Open in Google Maps"}/>
+        <Button link={"#intro"} buttonLabel={"Open in What3Words"}/>
+        <Button link={"#intro"} buttonLabel={"Open in Waze"}/>
+        <div id="accommodation" className="wrapper__content">
+          <h3>
+            Need accommodation?
+          </h3>
+          <p>
+            Here’s our suggestions for places to stay...
+          </p>
+          <Button link={"#intro"} buttonLabel={"View places"}/>
+        </div>
+      </div>
+      <Divider />
+      <div className="wrapper__content">
+        <h2>
+          The Day
+        </h2>
+        <ul className='p-lrg'>
+          <li>Ceremony: X – Xpm</li>
+          <li>Wedding Breakfast: X – Xpm</li>
+          <li>Evening reception: X – 11pm</li>
+          <li>Carriages: 11.30pm</li>
+        </ul>
+      </div>
+      <Divider />
+      <div className="wrapper__content">
+        <h2>
+          Wedding Gifts
+        </h2>
+        <p className="p-lrg">
+          Your presence and company at our special day is enough.
+        </p>
+        <p>
+          If you’d like to contribute to one of our selected gifts or causes, we would consider this a huge act of generosity that we would be forever grateful for.
+        </p>
+        <Button link={"#intro"} buttonLabel={"Contribute gifts"}/>
+      </div>
+      <Divider />
+      <div className="wrapper__content">
+        <h2>
+          R.S.V.P
+        </h2>
+        <p className="p-lrg">
+          Please let us know if you’re joining us on our special day.
+        </p>
+        <p>
+          Make sure to R.S.V.P for each person on your invitation. This enables us to understand numbers and dietary requirements.
+        </p>
+        <Button link={"#intro"} buttonLabel={"Ceremony & evening"}/>
+        <Button link={"#intro"} buttonLabel={"Evening celebrations"}/>
+      </div>
+      <Divider />
+      <div className="wrapper__content">
+        <h2>
+          Contact
+        </h2>
+        <div>
+          <p className="p-lrg">
+            Isabelle
+          </p>
+          <p>
+            07XXX XXXXXX
+          </p>
+        </div>
+        <div>
+          <p className="p-lrg">
+            Renay
+          </p>
+          <p>
+            07XXX XXXXXX
+          </p>
+        </div>
+      </div>
+      <p style={{color:"#4e5e657c", fontSize:12}}>
+        © 2025. Designed and developed by Ben, the BIL.
+      </p>
     </>
   )
 }
