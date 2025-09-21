7a025cd47a727a6c8008e5aa7026c60472b1da89
Ben <thewavyblue@gmail.com>
2025-09-21 20:33:16 +0100

    v0.16

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -2,18 +2,34 @@ import React from 'react';
 import IMAGES from './img/Images';
 import LinkBtn from './LinkBtn';
 import NavBtn from './NavBtn';
+import { Link } from "react-router-dom";
+import { HashLink } from 'react-router-hash-link';
 import './NavBar.css'
 import './App.css'
 
-export default function NavBar() {
 
+const NavBar = () => {
   return (
-    <div className="main-container container__navbar">
-      <nav className="navbar">
-          <NavBtn link={"/"} buttonLabel={"Home"} />
-          <NavBtn link={"/accommodation"} buttonLabel={"Accommodation"} />
-          <NavBtn link={"/#rsvp"} buttonLabel={"R.S.V.P"} />
+    <div className="container__navbar">
+      <nav>
+        <ul className="navbar">
+          {/* Normal route navigation */}
+          <li className='button'>
+            <Link to="/">Home</Link>
+          </li>
+          <li className='button'>
+            <Link to="/accommodation">Accommodation</Link>
+          </li>
+          {/* Anchored section on homepage */}
+          <li className='button'>
+            <HashLink smooth to="/#rsvp">
+              R.S.V.P
+            </HashLink>
+          </li>
+        </ul>
       </nav>
     </div>
-  )
-}
+  );
+};
+
+export default NavBar;
